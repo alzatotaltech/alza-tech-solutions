e79fcc59a8a489d1e9028351aacb60ece2d98436
alzatotaltech <alzatotaltechsolutionsfzllc@gmail.com>
2026-08-07 16:24:14 +0400

    con

--- pages/docs/enterprise-checklist.html.tsx
+++
@@ -1,7 +0,0 @@
-import { MarketingPage } from "@/components/MarketingPage";
-import { pageData } from "@/lib/pageData";
-import { Content_docs_enterprise_checklist } from "@/components/content/docs_enterprise_checklist";
-
-function Page() { return <MarketingPage data={pageData["docs/enterprise-checklist.html"]}><Content_docs_enterprise_checklist/></MarketingPage>; }
-(Page as any).bodyClass = pageData["docs/enterprise-checklist.html"].bodyClass;
-export default Page;
--- pages/thank-you.html.tsx
+++
@@ -1,7 +0,0 @@
-import { MarketingPage } from "@/components/MarketingPage";
-import { pageData } from "@/lib/pageData";
-import { Content_thank_you } from "@/components/content/thank_you";
-
-function Page() { return <MarketingPage data={pageData["thank-you.html"]}><Content_thank_you/></MarketingPage>; }
-(Page as any).bodyClass = pageData["thank-you.html"].bodyClass;
-export default Page;
