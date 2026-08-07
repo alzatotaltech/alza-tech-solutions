7f1a67cfdfcfcc5331a3b7a6a8797ce92b542933
alzatotaltech <alzatotaltechsolutionsfzllc@gmail.com>
2026-08-07 19:04:36 +0400

    Add files via upload

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,4 +1,7 @@
 import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "ALZA Total Tech Solutions FZ-LLC",
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
+
 const routes = [
   "/",
   "/products/",
